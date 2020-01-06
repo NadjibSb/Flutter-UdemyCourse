1bf5a63789db4ae2bcce748d18694ae85ce77038
NadjibSb <fn_souab@esi.dz>
2020-01-06 12:38:05 +0100

    flutter layouts

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/ui/home.dart';
 import './ui/welcome_home.dart';
 
 void main() {
   runApp(new MaterialApp(
-    title: "Welcome App",
-    home: new Welcome(),
+    title: "Welcome App - Layouts",
+    //home: new Welcome(),
+    home: new Home(),
   ));
 }
 
---
+++ lib/ui/home.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        color: Colors.greenAccent,
+        alignment: Alignment.center,
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              "First item",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontWeight: FontWeight.w800, fontSize: 18),
+            ),
+            new Text(
+              "Second item",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+            ),
+            new Expanded(
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Text(
+                    "Thirsd item ",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(fontWeight: FontWeight.w800, fontSize: 18),
+                  ),
+                  new Text(
+                    "Fourth item ",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+                  ),
+                  new Text(
+                    "Fifth item ",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            new Stack(
+              alignment: Alignment.centerLeft,
+              children: <Widget>[
+                const Text(
+                  "ONE",
+                  style: TextStyle(color: Colors.red),
+                ),
+                const Text(
+                  "two",
+                  style: TextStyle(color: Colors.yellow),
+                ),
+              ],
+            )
+          ],
+        ));
+  }
+}
