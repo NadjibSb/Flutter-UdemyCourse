3b5be7cd2e80d37fa0ecce7f0f76414118e1c7f5
NadjibSb <fn_souab@esi.dz>
2020-01-27 13:36:26 +0100

    Flutter layouts

--- lib/ui/layouts.dart
+++ lib/ui/layouts.dart
@@ -1,38 +1,46 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Layouts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return new Container(
         color: Colors.greenAccent,
         alignment: Alignment.center,
+
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+
             new Text(
               "First item",
               textDirection: TextDirection.ltr,
               style: TextStyle(fontWeight: FontWeight.w800, fontSize: 18),
             ),
+
             new Text(
               "Second item",
               textDirection: TextDirection.ltr,
               style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
             ),
+
             new Expanded(
               child: new Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
+
                   new Text(
                     "Thirsd item ",
                     textDirection: TextDirection.ltr,
                     style: TextStyle(fontWeight: FontWeight.w800, fontSize: 18),
                   ),
+
                   new Text(
                     "Fourth item ",
                     textDirection: TextDirection.ltr,
                     style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
                   ),
+
                   new Text(
                     "Fifth item ",
                     textDirection: TextDirection.ltr,
@@ -41,13 +49,16 @@ class Home extends StatelessWidget {
                 ],
               ),
             ),
+
             new Stack(
               alignment: Alignment.centerLeft,
               children: <Widget>[
+
                 const Text(
                   "ONE",
                   style: TextStyle(color: Colors.red),
                 ),
+
                 const Text(
                   "two",
                   style: TextStyle(color: Colors.yellow),
